8227e5b8bf492d31a348859a8fed3eca731e321d
DongWoonKim <01030985843z@naver.com>
2024-10-31 13:49:23 +0900

    security

---
+++ src/main/java/com/example/spring/edgeservice/config/jwt/TokenAuthenticationFilter.java
@@ -0,0 +1,15 @@
+package com.example.spring.edgeservice.config.jwt;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class TokenAuthenticationFilter implements WebFilter {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return null;
+    }
+}
