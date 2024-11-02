24fd6a0efd3d632838b1955c25d188416b85e97d
DongWoonKim <01030985843z@naver.com>
2024-11-02 21:39:45 +0900

    update eks settings redis 제외

--- src/main/java/com/example/spring/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/example/spring/edgeservice/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.spring.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -26,7 +25,9 @@ public class SecurityConfig {
                                 "/books",
                                 "/books/**",
                                 "/orders",
-                                "/orders/**"
+                                "/orders/**",
+                                "/actuator/**",          // actuator 모든 엔드포인트 허용
+                                "/actuator/health/**"    // 명시적으로 health 엔드포인트 허용
                         ).permitAll()
                         .anyExchange().authenticated()
                 )
