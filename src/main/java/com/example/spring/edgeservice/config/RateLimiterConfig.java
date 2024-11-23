8df4cd5868df79c8f7e600a8e685de0dca4bb3eb
DongWoonKim <01030985843z@naver.com>
2024-11-23 16:00:23 +0900

    update code

--- src/main/java/com/example/spring/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/example/spring/edgeservice/config/RateLimiterConfig.java
@@ -1,8 +1,11 @@
 package com.example.spring.edgeservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import reactor.core.publisher.Mono;
 
 @Configuration
---
+++ src/main/java/com/example/spring/edgeservice/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.example.spring.edgeservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host:localhost}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port:6379}")
+    private int redisPort;
+
+    @Value("${spring.data.redis.username:user}")
+    private String redisUsername;
+
+    @Value("${spring.data.redis.password:1234}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setUsername(redisUsername); // 사용자 이름 설정
+        config.setPassword(redisPassword); // 비밀번호 설정
+        return new LettuceConnectionFactory(config);
+    }
+
+}
